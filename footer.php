1ecd96f955e154b7030fcfd50ac4c11e6b3ede46
dddmerz <dddmerz@gmail.com>
2026-06-27 20:33:50 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+</div>
+
+<footer class="footer">
+
+    <div class="footer-content">
+
+        <div>
+            © <?= date("Y") ?> DeMerzli Sammelkartenspiel
+        </div>
+
+        <div>
+            Version 2.0 Beta
+        </div>
+
+        <div>
+            ⚔ Arena Beta
+        </div>
+
+    </div>
+
+</footer>
+
+</body>
+</html>
